58b16851316cc831feeb9e3d6ca7e49b87417ded
ynjisng <abreusara012@gmail.com>
2024-12-07 12:33:40 -0300

    feature: cria os tipos de token

---
+++ src/lexer/TokenType.java
@@ -0,0 +1,20 @@
+package lexer;
+
+public class TokenType {
+
+    public enum TokenReserved {
+        BEGIN, END, VAR, INTEGER, BOOLEAN, DEF, DO, RETURN, IF, ELSE, WHILE, PRINT, FUNCTION, PROCEDURE, CONTINUE, BREAK, TRUE, FALSE;
+    }
+
+    public enum TokenSimple {
+        ID, NUMERO;
+    }
+
+    public enum TokenOperator {
+        MAIS, MENOS, VEZES, DIVIDIDO, IGUAL, ATRIBUICAO, DIFERENTE, MENOR, MENORIGUAL, MAIOR, MAIORIGUAL;
+    }
+
+    public enum TokenSymbol {
+        ABRE_PARENTESE, FECHA_PARENTESE, PONTO_E_VIRGULA, DOIS_PONTOS, VIRGULA;
+    }
+}
